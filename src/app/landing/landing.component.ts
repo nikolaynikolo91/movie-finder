34131e1e35e9cab8ecdc066ca6c3f7decd1ed823
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2020-10-31 21:59:27 +0200

    add movie search observable

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Movie from '../models/Movie';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
+  movieSearch$: Observable<Movie[]>;
+  constructor(private movieService: MovieService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  search(value): void {
+   // console.log(value.Search);
+    this.movieSearch$ = this.movieService.searchMovie(value.Search);
   }
-
 }
